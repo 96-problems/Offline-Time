7a393e10898a8a81c61f7650097966c5d7ce5891
Naoto Ida <Adi@Naotos-MacBook-Pro.local>
2015-09-28 18:08:13 +0900

    Uncommented wifi related code

--- Offline Time/PopupMenu.swift
+++ Offline Time/PopupMenu.swift
@@ -9,6 +9,7 @@
 import Cocoa
 
 protocol PopupMenuDelegate {
+    func onToggleStartup()
     func onRequestQuit()
 }
 
@@ -31,6 +32,10 @@ class PopupMenu: NSMenu {
     
     override func validateToolbarItem(theItem: NSToolbarItem) -> Bool {
         return true
+    }
+    
+    @IBAction func toggleStartup(sender:AnyObject) {
+        self.customDelegate?.onToggleStartup()
     }
     
     @IBAction func quitButtonPressed(sender: AnyObject) {
